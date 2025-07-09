31a4d266a5e25b28ec8493fca73e71837ef5af9c
Lucky Solanki <108862877+Luckysolanki902@users.noreply.github.com>
2025-07-09 17:55:43 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import InitialLoading from "./components/InitialLoading";
+import { Analytics} from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {/* <InitialLoading /> */}
+</ Analytics>
         {children}
       </body>
     </html>
